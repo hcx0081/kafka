739c0084f204976d9b134185cc6bebaea8306a68
hcx0081 <623363564@qq.com>
2023-12-09 12:29:41 +0800

    生产者

--- producer/src/main/java/com/kafka/producer/MyProducer.java
+++ producer/src/main/java/com/kafka/producer/MyProducer.java
@@ -1,21 +1,29 @@
-package com.kafka;
+package com.kafka.producer;
 
 import org.apache.kafka.clients.producer.*;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.Properties;
+import java.util.concurrent.ExecutionException;
 
 /**
  * {@code @description:}
  */
-public class CustomProducer {
-    public static void main(String[] args) {
+public class MyProducer {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
         Properties props = new Properties();
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.100.100:9092");
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         KafkaProducer<String, String> producer = new KafkaProducer<>(props);
         
+        /* 异步发送 */
+        // 没有回调函数的异步发送
+        for (int i = 0; i < 5; i++) {
+            producer.send(new ProducerRecord<>("one", "hello world"));
+        }
+        
+        // 具有回调函数的异步发送
         for (int i = 0; i < 5; i++) {
             producer.send(new ProducerRecord<>("one", "hello world"), new Callback() {
                 @Override
@@ -27,6 +35,12 @@ public class CustomProducer {
                     }
                 }
             });
+        }
+        
+        
+        /* 同步发送 */
+        for (int i = 0; i < 5; i++) {
+            producer.send(new ProducerRecord<>("one", "hello world")).get();
         }
         
         producer.close();
