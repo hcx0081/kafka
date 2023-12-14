ac2f5305d86c29a6b503898e87240e31c4d1d708
hcx0081 <623363564@qq.com>
2023-12-14 10:42:24 +0800

    消费者

---
+++ producer/src/main/java/com/kafka/customer/MyConsumer.java
@@ -0,0 +1,42 @@
+package com.kafka.customer;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Properties;
+
+/**
+ * {@code @description:}
+ */
+public class MyConsumer {
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.100.100:9092");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+        
+        // 订阅主题
+        // ArrayList<String> topicList = new ArrayList<>();
+        // topicList.add("one");
+        // consumer.subscribe(topicList);
+        
+        // 订阅分区
+        ArrayList<TopicPartition> topicPartitionList = new ArrayList<>();
+        topicPartitionList.add(new TopicPartition("one", 0));
+        consumer.assign(topicPartitionList);
+        
+        while (true) {
+            ConsumerRecords<String, String> consumerRecords = consumer.poll(Duration.ofSeconds(5));
+            for (ConsumerRecord<String, String> consumerRecord : consumerRecords) {
+                System.out.println(consumerRecord);
+            }
+        }
+    }
+}
--- producer/src/main/java/com/kafka/producer/MyProducer.java
+++ producer/src/main/java/com/kafka/producer/MyProducer.java
@@ -1,6 +1,8 @@
 package com.kafka.producer;
 
-import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.Properties;
@@ -20,28 +22,29 @@ public class MyProducer {
         /* 异步发送 */
         // 没有回调函数的异步发送
         for (int i = 0; i < 5; i++) {
-            producer.send(new ProducerRecord<>("one", "hello world"));
+            // producer.send(new ProducerRecord<>("one", "hello world"));
+            producer.send(new ProducerRecord<>("one", 0, "", "hello world"));
         }
         
-        // 具有回调函数的异步发送
-        for (int i = 0; i < 5; i++) {
-            producer.send(new ProducerRecord<>("one", "hello world"), new Callback() {
-                @Override
-                public void onCompletion(RecordMetadata metadata, Exception exception) {
-                    if (exception == null) {
-                        System.out.println("主题：" + metadata.topic() + "\t" +
-                                "分区：" + metadata.partition() + "\t" +
-                                "时间戳：" + metadata.timestamp());
-                    }
-                }
-            });
-        }
-        
-        
-        /* 同步发送 */
-        for (int i = 0; i < 5; i++) {
-            producer.send(new ProducerRecord<>("one", "hello world")).get();
-        }
+        // // 具有回调函数的异步发送
+        // for (int i = 0; i < 5; i++) {
+        //     producer.send(new ProducerRecord<>("one", "hello world"), new Callback() {
+        //         @Override
+        //         public void onCompletion(RecordMetadata metadata, Exception exception) {
+        //             if (exception == null) {
+        //                 System.out.println("主题：" + metadata.topic() + "\t" +
+        //                         "分区：" + metadata.partition() + "\t" +
+        //                         "时间戳：" + metadata.timestamp());
+        //             }
+        //         }
+        //     });
+        // }
+        //
+        //
+        // /* 同步发送 */
+        // for (int i = 0; i < 5; i++) {
+        //     producer.send(new ProducerRecord<>("one", "hello world")).get();
+        // }
         
         producer.close();
     }
